281d3b8ea1b27703ae5420be87241094a27bd22a
tonytonyhung <leduyhung251192@gmail.com>
2018-11-04 15:10:48 +0700

    update sliding

--- app/src/main/java/leduyhung/me/vocaloid/custom/SlidingLayout/SlidingLayout.java
+++ app/src/main/java/leduyhung/me/vocaloid/custom/SlidingLayout/SlidingLayout.java
@@ -1,31 +1,54 @@
 package leduyhung.me.vocaloid.custom.SlidingLayout;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-public class SlidingView extends ScrollView implements ViewTreeObserver.OnGlobalLayoutListener {
+import com.leduyhung.loglibrary.Logg;
+
+import leduyhung.me.vocaloid.R;
+
+public class SlidingLayout extends ScrollView implements ViewTreeObserver.OnGlobalLayoutListener,
+        ViewTreeObserver.OnScrollChangedListener {
+
+    private final int POINT_TO_SCROLL_CHANGE_STATE = 150;
+    private final int TIME_SMOOTH_SCROLL = 200;
 
     private Context mContext;
 
-    private FrameLayout frameParent, frameContent;
+    private LinearLayout linearParent;
+    private FrameLayout frameContent;
+    private View vOverlay;
 
-    private int width, height;
+    private boolean isTouch;
+    private int width, height, heightToTouch;
+    private float maxOverlay;
+    private int maxScroll, currentY;
 
-    public SlidingView(Context context) {
+    private SlidingState.STATE state;
+
+    public SlidingLayout(Context context) {
         super(context);
         getAtribute(context, null);
     }
 
-    public SlidingView(Context context, AttributeSet attrs) {
+    public SlidingLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         getAtribute(context, attrs);
     }
 
-    public SlidingView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SlidingLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         getAtribute(context, attrs);
     }
@@ -35,6 +58,7 @@ public class SlidingView extends ScrollView implements ViewTreeObserver.OnGlobal
         super.onLayout(changed, l, t, r, b);
         this.width = getWidth();
         this.height = getHeight();
+        maxScroll = height - heightToTouch;
     }
 
     @Override
@@ -48,6 +72,31 @@ public class SlidingView extends ScrollView implements ViewTreeObserver.OnGlobal
     @Override
     public void onGlobalLayout() {
 
+        getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        initView();
+        setVerticalScrollBarEnabled(false);
+    }
+
+    @Override
+    public void onScrollChanged() {
+
+        currentY = getScrollY();
+        vOverlay.setAlpha(calculateAlphaOverlay(currentY));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (state == SlidingState.STATE.SCROLLING) {
+                    return false;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                handleScrollWhenTouch();
+                break;
+        }
+        return super.onTouchEvent(ev);
     }
 
     private void getAtribute(Context mContext, AttributeSet attributeSet) {
@@ -56,5 +105,92 @@ public class SlidingView extends ScrollView implements ViewTreeObserver.OnGlobal
         if (attributeSet != null) {
 
         }
+
+        maxOverlay = 0.8f;
+        heightToTouch = 0;
+        state = SlidingState.STATE.COLLAPSE;
+        getViewTreeObserver().addOnScrollChangedListener(this);
+    }
+
+    private void initView() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearParent = new LinearLayout(mContext);
+        linearParent.setOrientation(LinearLayout.VERTICAL);
+        linearParent.setLayoutParams(layoutParams);
+        vOverlay = new View(mContext);
+        LayoutParams params = new LayoutParams(width, height);
+        vOverlay.setLayoutParams(params);
+        vOverlay.setBackgroundColor(mContext.getResources().getColor(R.color.colorblack));
+        vOverlay.setAlpha(0f);
+        frameContent = new FrameLayout(mContext);
+        frameContent.setBackgroundColor(mContext.getResources().getColor(R.color.colorAccent));
+        frameContent.setLayoutParams(params);
+        linearParent.addView(vOverlay);
+        linearParent.addView(frameContent);
+        addView(linearParent);
+    }
+
+    private float calculateAlphaOverlay(int y) {
+
+        return y * maxOverlay / maxScroll;
+    }
+
+    private void handleScrollWhenTouch() {
+
+        if (state == SlidingState.STATE.EXPANSE) {
+            if (currentY > POINT_TO_SCROLL_CHANGE_STATE)
+                close();
+            else
+                open();
+        } else if (state == SlidingState.STATE.COLLAPSE) {
+            if (currentY < maxScroll - POINT_TO_SCROLL_CHANGE_STATE)
+                open();
+            else
+                close();
+        } else {
+
+        }
+    }
+
+    private void smoothScroll(int y, final SlidingState.STATE newState) {
+        ValueAnimator animScroll =
+                ValueAnimator.ofInt(currentY, y);
+        animScroll.setDuration(TIME_SMOOTH_SCROLL);
+        animScroll.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                state = SlidingState.STATE.SCROLLING;
+                smoothScrollTo(0, (int) valueAnimator.getAnimatedValue());
+            }
+        });
+        animScroll.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                state = newState;
+            }
+        });
+        animScroll.start();
+    }
+
+    public void open() {
+        if (state == SlidingState.STATE.COLLAPSE)
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    smoothScroll(0, SlidingState.STATE.EXPANSE);
+                }
+            });
+    }
+
+    public void close() {
+        if (state == SlidingState.STATE.EXPANSE)
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    smoothScroll(maxScroll, SlidingState.STATE.COLLAPSE);
+                }
+            });
     }
 }
--- app/src/main/java/leduyhung/me/vocaloid/custom/SlidingLayout/SlidingState.java
+++ app/src/main/java/leduyhung/me/vocaloid/custom/SlidingLayout/SlidingState.java
@@ -1,4 +1,8 @@
 package leduyhung.me.vocaloid.custom.SlidingLayout;
 
 public class SlidingState {
+
+    enum STATE {
+        EXPANSE, COLLAPSE, SCROLLING
+    }
 }
