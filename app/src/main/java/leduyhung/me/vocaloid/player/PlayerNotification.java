73a35e792f1e138c1b2eafdf71408c767dcff9de
tonytonyhung <leduyhung251192@gmail.com>
2018-11-07 23:07:29 +0700

    update custom layout notification

--- app/src/main/java/leduyhung/me/vocaloid/player/PlayerNotification.java
+++ app/src/main/java/leduyhung/me/vocaloid/player/PlayerNotification.java
@@ -3,8 +3,10 @@ package leduyhung.me.vocaloid.player;
 import android.app.Notification;
 import android.content.Context;
 import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 import leduyhung.me.vocaloid.R;
+import leduyhung.me.vocaloid.util.ClientUtil;
 
 public class PlayerNotification {
 
@@ -13,18 +15,20 @@ public class PlayerNotification {
 
     private Context mContext;
     private NotificationCompat.Builder mBuilder;
+    private RemoteViews remoteViews;
 
     public PlayerNotification(Context mContext) {
         this.mContext = mContext;
     }
 
     public Notification createNotification() {
+
+        remoteViews = new RemoteViews(ClientUtil.getPakageName(mContext), R.layout.layout_media_notification);
+
         mBuilder = new NotificationCompat.Builder(mContext, CHANNE_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Title")
-                .setContentText("Content")
-                .setCategory(Notification.CATEGORY_PROGRESS)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCustomBigContentView(remoteViews);
         return mBuilder.build();
     }
 }
