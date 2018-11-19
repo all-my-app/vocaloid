84571a3e9b414291b78f3105ec0df8e023dec76e
tonytonyhung <leduyhung251192@gmail.com>
2018-11-19 21:52:35 +0700

    update seekbar

--- app/src/main/java/leduyhung/me/vocaloid/TestActivity.java
+++ app/src/main/java/leduyhung/me/vocaloid/TestActivity.java
@@ -6,38 +6,54 @@ import android.os.Bundle;
 
 import com.leduyhung.loglibrary.Logg;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import leduyhung.me.vocaloid.custom.Seekbar.SeekbarView;
 import leduyhung.me.vocaloid.custom.SlidingLayout.SlidingLayout;
 import leduyhung.me.vocaloid.custom.SlidingLayout.SlidingLayoutListener;
 import leduyhung.me.vocaloid.custom.SlidingLayout.SlidingState;
 
 public class TestActivity extends AppCompatActivity {
 
-    private SlidingLayout slidingLayout;
+//    private SlidingLayout slidingLayout;
+    private SeekbarView seekbarView;
+    private int progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        slidingLayout = findViewById(R.id.sliding_layout);
-        slidingLayout.setSlidingLayoutListener(new SlidingLayoutListener() {
-            @Override
-            public void onScrollChange(int y, SlidingState.STATE state) {
-
-            }
-        });
-
-        Handler handler = new Handler(getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        slidingLayout.setHeightToTouch(200);
-                    }
-                });
-            }
-        }, 2000);
+        seekbarView = findViewById(R.id.seekbar);
+//        Timer t = new Timer();
+//        TimerTask tTask = new TimerTask() {
+//            @Override
+//            public void run() {
+//                progress++;
+//                seekbarView.setProgress(progress);
+//            }
+//        };
+//        t.schedule(tTask, 500, 500);
+//        slidingLayout = findViewById(R.id.sliding_layout);
+//        slidingLayout.setSlidingLayoutListener(new SlidingLayoutListener() {
+//            @Override
+//            public void onScrollChange(int y, SlidingState.STATE state) {
+//
+//            }
+//        });
+//
+//        Handler handler = new Handler(getMainLooper());
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        slidingLayout.setHeightToTouch(200);
+//                    }
+//                });
+//            }
+//        }, 2000);
     }
 }
--- app/src/main/java/leduyhung/me/vocaloid/custom/Seekbar/SeekbarView.java
+++ app/src/main/java/leduyhung/me/vocaloid/custom/Seekbar/SeekbarView.java
@@ -2,13 +2,25 @@ package leduyhung.me.vocaloid.custom.Seekbar;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import leduyhung.me.vocaloid.R;
 
 public class SeekbarView extends View {
 
     private Context mContext;
+    private int width, height, currentSeek, oldSeek;
+    private int colorSeek, colorBackground;
+    private int maxProgress, progress;
+    private boolean changeFromUser;
+
+    private Paint paintSeek, paintBackground;
 
     public SeekbarView(Context context) {
         super(context);
@@ -28,11 +40,29 @@ public class SeekbarView extends View {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
+        this.width = getWidth();
+        this.height = getHeight();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawBackground(canvas);
+        drawSeek(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+            changeFromUser = true;
+            this.currentSeek = (int) event.getX();
+            if (currentSeek != oldSeek) {
+                postInvalidate();
+            }
+        } else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+            changeFromUser = false;
+        }
+        return true;
     }
 
     private void getAttribute(Context mContext, AttributeSet attrs) {
@@ -41,6 +71,44 @@ public class SeekbarView extends View {
 
         if (attrs != null) {
 
+        }
+        progress = 0;
+        maxProgress = 100;
+
+        colorSeek = mContext.getResources().getColor(R.color.colorAccent);
+        colorBackground = mContext.getResources().getColor(R.color.colorGray);
+
+        paintSeek = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintSeek.setColor(colorSeek);
+
+        paintBackground = new Paint();
+        paintBackground.setColor(colorBackground);
+        changeFromUser = false;
+    }
+
+    private void drawBackground(Canvas canvas) {
+
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.ADD);
+        canvas.drawRect(0, 0, width, height, paintBackground);
+    }
+
+    private void drawSeek(Canvas canvas) {
+
+        calculateCurrentSeek();
+        canvas.drawRect(0, 0, currentSeek, height, paintSeek);
+    }
+
+    private void calculateCurrentSeek() {
+        if (maxProgress != 0 && !changeFromUser) {
+            currentSeek = progress * width / maxProgress;
+        }
+        oldSeek = currentSeek;
+    }
+
+    public void setProgress(int progress) {
+        if (progress <= maxProgress) {
+            this.progress = progress;
+            postInvalidate();
         }
     }
 }
